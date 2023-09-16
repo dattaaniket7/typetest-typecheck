c3091bf16b16c418f45da8825fcb7ba895a8450d
Aniket Datta <datta.aniket7@gmail.com>
2023-09-17 00:50:36 +0530

    object-property

---
+++ src/test.ts
@@ -0,0 +1,18 @@
+interface NPC {
+  health: number;
+}
+
+interface Enemy extends NPC {
+  power: number;
+}
+
+type Stranger = Enemy | NPC;
+
+function makeEnemy(stranger: Stranger) {
+  if ("power" in stranger) {
+    return { health: stranger.health, power: stranger.power };
+  }
+}
+
+const test = makeEnemy({ health: 5, power: 2 });
+console.log(test);
